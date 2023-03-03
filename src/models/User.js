0141e1c72c65fc9176401ad7b7f33d378e6013e8
Gilmara <gilmaralopes2006@gmail.com>
2023-03-03 17:31:38 -0300

    [Feat]:rocket: Requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+const User = (sequelize, DataTypes) => {
+  const user = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    display_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    tableName: 'users',
+    timestamps: false,
+  });
+
+  return user;
+}
+
+module.exports = User;
